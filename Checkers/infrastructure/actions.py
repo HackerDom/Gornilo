8cc1f053de392a30e6baf88f5a0b126832558fac
rx00 <it.tec@me.com>
2019-04-27 03:45:10 +0500

    fix chk imports + better example

--- Checkers/infrastructure/actions.py
+++ Checkers/infrastructure/actions.py
@@ -1,7 +1,4 @@
 import sys
-if sys.version_info[:2] < (3, 6):
-    raise RuntimeError("Python version should be 3.6+")
-
 import inspect
 from typing import Tuple, Type
 from .verdict import Verdict
