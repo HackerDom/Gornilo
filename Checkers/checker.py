4458333df50d5fcce9bc9375a7b0f3c05b0930bc
rx00 <it.tec@me.com>
2019-06-06 16:18:44 +0500

    single object in args

--- Checkers/checker.py
+++ Checkers/checker.py
@@ -1,40 +1,41 @@
 from infrastructure.actions import Checker
 from infrastructure.verdict import Verdict
+from infrastructure.checker_request import CheckRequest, PutRequest, GetRequest
 
 
 Checker.INFO = "1:2"  # means vulns distribution
 
 
 @Checker.define_check
-def check_service(host: str) -> Verdict:
+def check_service(request: CheckRequest) -> Verdict:
     ...  # your code
 
     return Verdict.OK()
 
 
 @Checker.define_put(vuln_num=1)
-def put_flag_into_the_service(host: str, flag_id: str, flag: str) -> Verdict:
+def put_flag_into_the_service(request: PutRequest) -> Verdict:
     ...  # your code
 
     return Verdict.OK("my_new_flag_id")
 
 
 @Checker.define_get(vuln_num=1)
-def get_flag_from_the_service(host: str, flag_id: str, flag: str) -> Verdict:
+def get_flag_from_the_service(request: GetRequest) -> Verdict:
     ...  # your code
 
     return Verdict.OK()
 
 
 @Checker.define_put(vuln_num=2)
-def put_flag_into_the_service(host: str, flag_id: str, flag: str) -> Verdict:
+def put_flag_into_the_service(request: PutRequest) -> Verdict:
     ...  # your code
 
     return Verdict.MUMBLE("something bad with ur proto", "they are cheating!")
 
 
 @Checker.define_get(vuln_num=2)
-def get_flag_from_the_service(host: str, flag_id: str, flag: str) -> Verdict:
+def get_flag_from_the_service(request: GetRequest) -> Verdict:
     ...  # your code
 
     return Verdict.CORRUPT("flag lost", "lol, they lost it")
--- Checkers/infrastructure/actions.py
+++ Checkers/infrastructure/actions.py
@@ -1,11 +1,10 @@
 import sys
 import inspect
-from typing import Tuple, Type
-from .verdict import Verdict
+import datetime
 from traceback import format_exc
-
-
-CHECK, PUT, GET, INFO = "CHECK", "PUT", "GET", "INFO"
+from .checker_request import CheckRequest, PutRequest, GetRequest
+from .verdict import Verdict
+from .action_names import INFO, CHECK, PUT, GET, TEST
 
 
 class Checker:
@@ -18,26 +17,21 @@ class Checker:
     }
 
     @staticmethod
-    def __check_function(func: callable, annotations_mask=None or Tuple[Type]):
+    def __check_function(func: callable, func_type: type):
         func_name = func.__code__.co_name
         func_args_spec = inspect.getfullargspec(func)
 
         if func_args_spec.annotations.get("return") != Verdict:
             raise TypeError(f"Checker function ({func_name}) should return {Verdict} object!")
 
-        if annotations_mask is None:
-            return
+        if len(func_args_spec.args) < 1:
+            raise TypeError(f"{func_name} should have 1 or more args!")
 
-        if len(annotations_mask) != len(func_args_spec.args):
-            raise TypeError(f"Checker function ({func_name}) should have {len(annotations_mask)} arg(s)!")
+        func_arg_name = func_args_spec.args[0]
+        func_arg_type = func_args_spec.annotations.get(func_arg_name)
 
-        for i, annotated_type in enumerate(annotations_mask):
-            current_arg = func_args_spec.args[i]
-            current_arg_type = func_args_spec.annotations.get(current_arg)
-            if current_arg_type != annotated_type:
-                raise TypeError(f"Checker function ({func_name}) argument type at {i + 1} pos "
-                                f"should be {annotated_type}, "
-                                f"not {current_arg_type}!")
+        if func_arg_type != func_type:
+            raise TypeError(f"{func_name} first arg should be typed as {func_type}")
 
     @classmethod
     def __register_action(cls, action_name: str, action: callable, action_period: int = None):
@@ -54,8 +48,15 @@ class Checker:
                 raise ValueError("Incorrect action name!")
 
     @staticmethod
+    def __run_tests(team_ip):
+        ...
+        # todo run info-check-put-get actions + check runnability
+
+        # todo reality test (15 mins chk-system like test) & fast methods test
+
+    @staticmethod
     def define_check(func: callable) -> callable:
-        Checker.__check_function(func, (str,))
+        Checker.__check_function(func, CheckRequest)
         Checker.__register_action(CHECK, func)
         return func
 
@@ -65,7 +66,7 @@ class Checker:
             raise TypeError(f'You should provide vulnerability natural number as a decorator argument!')
 
         def wrapper(func: callable):
-            Checker.__check_function(func, (str, str, str))
+            Checker.__check_function(func, PutRequest)
             Checker.__register_action(PUT, func, vuln_num)
             return func
         return wrapper
@@ -76,7 +77,7 @@ class Checker:
             raise TypeError(f'You should provide vulnerability natural number as a decorator argument!')
 
         def wrapper(func: callable):
-            Checker.__check_function(func, (str, str, str))
+            Checker.__check_function(func, GetRequest)
             Checker.__register_action(GET, func, vuln_num)
             return func
         return wrapper
@@ -103,7 +104,7 @@ class Checker:
 
     @staticmethod
     def __run(command=None, hostname=None, flag_id=None, flag=None, vuln_id=None) -> Verdict:
-        commands = [CHECK, PUT, GET, INFO]
+        commands = [CHECK, PUT, GET, INFO, TEST]
 
         if command is None:
             raise ValueError("Expected 1 or more args!")
@@ -120,9 +121,13 @@ class Checker:
             raise ValueError("Can't find 'hostname' arg! (Expected 2 or more args)")
 
         check_func = Checker.__actions_handlers[CHECK]
+        request_content = {
+            "hostname": hostname
+        }
+
         if command == CHECK:
             # noinspection PyCallingNonCallable
-            return check_func(hostname)
+            return check_func(CheckRequest(**request_content))
 
         if flag_id is None:
             raise ValueError("Can't find 'flag_id' arg! (Expected 3 or more args)")
@@ -143,10 +148,16 @@ class Checker:
 
         put_func = Checker.__actions_handlers[PUT][vuln_id]
         get_func = Checker.__actions_handlers[GET][vuln_id]
+
+        request_content.update({
+            "flag_id": flag_id,
+            "flag": flag
+        })
+
         if command == PUT:
-            return put_func(hostname, flag_id, flag)
+            return put_func(PutRequest(**request_content))
 
         if command == GET:
-            return get_func(hostname, flag_id, flag)
+            return get_func(GetRequest(**request_content))
 
         raise RuntimeError("Something gone wrong with checker scenario :(")
