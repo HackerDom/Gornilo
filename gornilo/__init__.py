1c119f98a21bd31e6086e7eeab5c32ee8ab5b048
rx00 <rx00@hackerdom.ru>
2022-05-20 23:49:10 +0500

    rm log from init

--- gornilo/__init__.py
+++ gornilo/__init__.py
@@ -1,4 +1,3 @@
 from gornilo.actions import Checker, Verdict, CheckRequest, PutRequest, GetRequest
 from gornilo.new_actions import NewChecker
 from gornilo.sub_checker import VulnChecker
-from gornilo.setup_logging import setup_logging
